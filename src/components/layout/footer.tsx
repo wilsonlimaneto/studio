6290cb73605d0ca75915c12f508cc2bf239ef16b
Contato Maestria <contato@maestrialaw.com.br>
2025-05-15 14:22:24 +0000

    o logotipo maestria da seção final troque pela imagem tal qual a do topo

--- src/components/layout/footer.tsx
+++ src/components/layout/footer.tsx
@@ -1,4 +1,3 @@
-import Logo from '@/components/landing/logo';
 import { Youtube, Instagram, Linkedin } from 'lucide-react';
 import Link from 'next/link';
 
@@ -9,7 +8,9 @@ const Footer = () => {
       <div className="container mx-auto px-4 sm:px-6 lg:px-8 py-12">
         <div className="grid grid-cols-1 md:grid-cols-3 gap-8">
           <div>
-            <Logo svgClassName="h-7" />
+            <Link href="/" className="flex items-center">
+              <img src="/oie_pnD9PzjNbeOy.png" alt="Maestria Logo" className="h-6" />
+            </Link>
             <p className="mt-2 text-sm text-muted-foreground">
               Craft Intelligent Landing Pages, Effortlessly.
             </p>
