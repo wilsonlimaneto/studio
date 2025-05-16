d32c51b1f77bea95f7927f5af81fd90ac0c55092
Cloud Maestria <cloud@maestrialaw.com.br>
2025-05-16 22:24:19 +0000

    tag manager

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -38,8 +38,22 @@ export default function RootLayout({
             `,
           }}
         />
-      </head>
-      <body className={geistSans.variable + ' ' + geistMono.variable + ' antialiased font-sans'}>
+        {/* Google Tag Manager */}
+        <script type="text/javascript"
+          dangerouslySetInnerHTML={{
+            __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+                new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+                j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+                'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+                })(window,document,'script','dataLayer','GTM-WL9R9DLN');`,
+          }}
+        />
+
+              </head>
+              <body className={geistSans.variable + ' ' + geistMono.variable + ' antialiased font-sans'}>
+                <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-WL9R9DLN"
+                height="0" width="0" style={{ display: 'none', visibility: 'hidden' }}></iframe></noscript>
+                {/* End Google Tag Manager (noscript) */}
         {children}
         <Toaster />
       </body>
